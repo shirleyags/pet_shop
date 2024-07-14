406f519f4d99714df5b6a1342abcbb315eecd050
ShirleyAlice <shirleyags@yahoo.com.br>
2024-07-13 22:52:02 -0300

    Retifica indentação

--- admin/controller/clientList-controller.js
+++ admin/controller/clientList-controller.js
@@ -3,14 +3,18 @@ import { clientService } from '../service/clientService.js'
 const createNewLine = (name, email) => {
   const lineNewClient = document.createElement('tr')
   const content = 
-  `<td class="td" data-td>${name}</td>
-  <td>${email}</td>
-  <td>
-    <ul class="tabela__botoes-controle">
-      <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-      <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-    </ul>
-  </td>`
+    `<td class="td" data-td>${name}</td>
+    <td>${email}</td>
+    <td>
+      <ul class="tabela__botoes-controle">
+        <li>
+          <a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a>
+        </li>
+        <li>
+          <button class="botao-simples botao-simples--excluir" type="button">Excluir</button>
+        </li>
+      </ul>
+    </td>`
   lineNewClient.innerHTML = content
   return lineNewClient
 }
