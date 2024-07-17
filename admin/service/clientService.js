89ed4c95cbf4684a87d2c72deb9c862f477c9a9f
ShirleyAlice <shirleyags@yahoo.com.br>
2024-07-16 21:58:59 -0300

    Cria requisição post

--- admin/service/clientService.js
+++ admin/service/clientService.js
@@ -1,8 +1,39 @@
 const clientList = async () => {
-  const response = await fetch(`http://localhost:3000/profile`)
-  return response.json()
+  try {
+    const response = await fetch(`http://localhost:3000/profile`)
+    if (!response.ok) {
+      throw new Error('Não foi possível listar os clientes')
+    }
+    return response.json()
+  } catch (error) {
+    throw error
+  }
+}
+
+const createClient = async (name, email) => {
+  try {
+    const response = await fetch(`http://localhost:3000/profile`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+
+    if (!response.ok) {
+      throw new Error('Não foi possível cadastrar o cliente')
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const clientService = {
   clientList,
+  createClient
 }
