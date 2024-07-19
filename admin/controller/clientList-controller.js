150e44cbb28646cee975eb088cde769c135d705e
ShirleyAlice <shirleyags@yahoo.com.br>
2024-07-19 01:06:59 -0300

    Cria evento de exclusão do cliente e da linha

--- admin/controller/clientList-controller.js
+++ admin/controller/clientList-controller.js
@@ -1,6 +1,6 @@
 import { clientService } from '../service/clientService.js'
 
-const createNewLine = (name, email) => {
+const createNewLine = (name, email, id) => {
   const lineNewClient = document.createElement('tr')
   const content = `<td class="td" data-td>${name}</td>
     <td>${email}</td>
@@ -15,16 +15,32 @@ const createNewLine = (name, email) => {
       </ul>
     </td>`
   lineNewClient.innerHTML = content
+  lineNewClient.dataset.id = id
   return lineNewClient
 }
 
 const table = document.querySelector('[data-tabela]')
 
+table.addEventListener('click', async (event) => {
+  const deleteButton =
+    event.target.className === 'botao-simples botao-simples--excluir'
+  if (deleteButton) {
+    const clientLine = event.target.closest('[data-id]')
+    const id = clientLine.dataset.id
+    try {
+      await clientService.removeClient(id)
+      clientLine.remove()
+    } catch (error) {
+      console.error('Não foi possível remover o cliente:', error.message)
+    }
+  }
+})
+
 const loadClientList = async () => {
   try {
     const data = await clientService.clientList()
     data.forEach((element) => {
-      table.appendChild(createNewLine(element.name, element.email))
+      table.appendChild(createNewLine(element.name, element.email, element.id))
     })
   } catch (error) {
     console.error('Não foi possível listar os clientes:', error.message)
